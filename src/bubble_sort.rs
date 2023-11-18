3dcf4f410063ecc5ea4dba5dee85f435fef24428
pokotylo <pokotilo_a@tochka.com>
2023-11-18 19:46:02 +0600

    feat: adding sorting algorithms

---
+++ src/bubble_sort.rs
@@ -0,0 +1,11 @@
+fn bubble_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+
+    for i in 0..len {
+        for j in 0..len - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
---
+++ src/insertion_sort.rs
@@ -0,0 +1,12 @@
+fn insertion_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+
+    for i in 1..len {
+        let mut j = i;
+
+        while j > 0 && arr[j] < arr[j - 1] {
+            arr.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+}
