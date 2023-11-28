ca0e20ded6cb6adc58b4e068b8e9e712bb52ad2c
pokotylo <pokotilo_a@tochka.com>
2023-11-28 21:34:27 +0600

    fix: quicksort algorithm

--- src/quicksort.rs
+++ src/quicksort.rs
@@ -1,31 +1,31 @@
-pub fn quicksort<T: Ord>(vec: &mut [T]) {
+pub fn quicksort<T: Ord + std::fmt::Debug>(vec: &mut [T]) {
     if vec.len() <= 1 {
         return;
     }
     
     let q = partition(vec);
+    
     quicksort(&mut vec[0..q]);
-    quicksort(&mut vec[q + 1..]);
+    quicksort(&mut vec[q + 1..])
     
 }
 
-fn partition<T: Ord>(vec: &mut [T]) -> usize {
+fn partition<T: Ord + std::fmt::Debug>(vec: &mut [T]) -> usize {
+
+    let pivot_index = vec.len() / 2;
     
-    let r = vec.len() - 1;
+    let p: usize = vec.len() - 1;
     
-    let pivot = vec.len() / 2;
-    
-    vec.swap(pivot, r);
-    
+    vec.swap(pivot_index, p);
+
     let mut i = 0;
-    
-    for j in 0..r {
-        if vec[j] <= vec[pivot] {
+    for j in 0..p {
+        if vec[j] <= vec[p] {
             vec.swap(i, j);
             i += 1;
-            
         }
     }
-    vec.swap(i, r);
-    i 
+
+    vec.swap(i, p);
+    i
 }
